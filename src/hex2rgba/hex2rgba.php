02b868916a1c233e0486c25fcc3ea30f9e00767e
frnxstd <burak@myself.com>
2018-08-01 01:28:56 +0300

    initial commit

---
+++ src/hex2rgba/hex2rgba.php
@@ -0,0 +1,92 @@
+<?php
+namespace hex2rgba;
+
+use Exception;
+
+class hex2rgba
+{
+    /**
+     * @var string $default
+     */
+    public $default = 'rgb(0,0,0)';
+
+    /**
+     * @var string $hex
+     */
+    public $hex     = '000000';
+
+    /**
+     * @var array $convert
+     */
+    public $convert  = [];
+
+    /**
+     * @param $code
+     * @return string
+     */
+    public function sanitize($code)
+    {
+        if (substr($code, 0, 1) == '#')
+        {
+            $code = substr($code, 1);
+        }
+
+        $this->hex = $code;
+
+        // Just for using the function stand-alone, not necessary in this project
+        return $this->hex;
+    }
+
+    /**
+     * Determinate hexadecimal code length to normalize
+     *
+     * @throws Exception
+     */
+    public function normalize()
+    {
+        if (strlen($this->hex) == 6)
+        {
+            $this->convert = array( $this->hex[0] . $this->hex[1], $this->hex[2] . $this->hex[3], $this->hex[4] . $this->hex[5] );
+        } elseif (strlen($this->hex ) == 3)
+        {
+            $this->convert = array( $this->hex[0] . $this->hex[0], $this->hex[1] . $this->hex[1], $this->hex[2] . $this->hex[2] );
+        } else {
+            throw new Exception('Misformatted value: '.$this->hex);
+        }
+    }
+
+    /**
+     * Hexadecimal to rgba converter
+     *
+     * @param string $hex
+     * @param int    $opacity
+     * @return string
+     *
+     * @throws Exception
+     */
+    public function convert($hex = '', $opacity = 1)
+    {
+        if(!empty($hex) && is_string($hex))
+        {
+            $this->sanitize($hex);
+        }
+
+        $this->normalize();
+
+        //Convert hexadec to rgb
+        $rgb = array_map('hexdec', $this->convert);
+
+        //Check if opacity is set(rgba or rgb)
+        if(is_int($opacity))
+        {
+            if(abs($opacity) > 1)
+                $opacity = 1.0;
+
+            $output = 'rgba('.implode(',', $rgb).','.$opacity.')';
+        } else {
+            $output = 'rgba('.implode(',', $rgb).','.$opacity.')';
+        }
+
+        return $output;
+    }
+}
